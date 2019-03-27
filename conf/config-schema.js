3620c3a62a6c38ad7415436c9f24ac561427a452
Toru Nagashima <star.ctor@gmail.com>
2019-03-27 17:04:41 +0900

    New: implement Plugin Renaming
    https://github.com/eslint/rfcs/blob/eslintrc-improvements/designs/2019-eslintrc-improvements/minor-03-plugin-renaming.md

--- conf/config-schema.js
+++ conf/config-schema.js
@@ -16,7 +16,15 @@ const baseConfigProperties = {
     },
     parser: { type: ["string", "null"] },
     parserOptions: { type: "object" },
-    plugins: { type: "array" },
+    plugins: {
+        anyOf: [
+            { type: "array" },
+            {
+                type: "object",
+                additionalProperties: { type: "string" }
+            }
+        ]
+    },
     processor: { type: "string" },
     rules: { type: "object" },
     settings: { type: "object" },
